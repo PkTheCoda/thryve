5e64ee828521ee2398912c0d73e826f21b9e2bff
PkTheCoda <pranavkonjeti@gmail.com>
2025-03-01 20:19:44 -0500

    added 2 new testimonials

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Testimonial = () => {
   return (
     <div className='bg-main py-20 font-outfit'>
-        <div class="w-full flex justify-center items-center dark:bg-gray-700">
+        <div class="w-full flex flex-col justify-center items-center dark:bg-gray-700 gap-14">
 
         <section class="dark:bg-gray-800">
             <div class="max-w-6xl px-4 mx-auto text-center lg:px-6">
@@ -15,7 +15,7 @@ const Testimonial = () => {
                             fill="currentColor"></path>
                     </svg>
                     <blockquote>
-                        <p class="five-title font-medium text-gray-700 dark:text-white leading-loose lg:leading-[3.8rem]">
+                        <p class="five-title font-medium text-gray-700 dark:text-white leading-normal lg:leading-[3.8rem]">
                             I had a smaller budget, but Thryve{' '}
                             <span className="testimonial-block">exceeded my expectations.</span>. Within just a few days, Pranav and his team created an 
                             amazing homepage for my lawn-mowing business and setup everything from forms to analytics
@@ -32,6 +32,59 @@ const Testimonial = () => {
                 </figure>
             </div>
         </section>
+
+
+
+
+        <div className='w-full max-w-6xl grid md:grid-cols-2 gap-10 px-8'>
+
+            <figure class="relative rounded-2xl bg-white p-6 shadow-xl shadow-slate-900/10 md:-rotate-1">
+                <svg aria-hidden="true" width="105" height="78" class="absolute top-6 left-6 fill-slate-100">
+                    <path
+                        d="M25.086 77.292c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622C1.054 58.534 0 53.411 0 47.686c0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C28.325 3.917 33.599 1.507 39.324 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Zm54.24 0c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622-2.11-4.52-3.164-9.643-3.164-15.368 0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C82.565 3.917 87.839 1.507 93.564 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Z">
+                    </path>
+                </svg>
+                <blockquote class="relative">
+                    <p class="text-lg tracking-tight text-slate-700">
+                        Thryve was fantastic to work with! I gave them a few simple ideas for my dog grooming website, 
+                        and Pranav and his team turned them into a fun, visually appealing site that captures my brand 
+                        perfectly for an affordable price. Would highly recommend for any small business owner.
+                    </p>
+                </blockquote>
+                <figcaption class="relative mt-6 flex items-center justify-between border-t border-slate-100 pt-6">
+                    <div>
+                        <div class="font-display text-base text-slate-900">Anayah R.</div>
+                        <div class="mt-1 text-sm text-slate-500">Founder @ <a target='_blank' href="https://pawsandrelax.netlify.app" className='text-blue-400 underline'>Paws and Relax</a></div>
+                    </div>
+                    <div class="overflow-hidden rounded-full bg-slate-50">
+                        <img alt="" src="https://pawsandrelax.netlify.app/assets/pawsandrelaxlogo-CPFHZbo-.png" class="h-14 w-14 object-cover" loading="lazy" width="56" height="56"/>
+                    </div>
+                </figcaption>
+            </figure>
+
+            <figure class="relative rounded-2xl bg-white p-6 shadow-xl shadow-slate-900/10 md:rotate-1 md:top-4">
+                <svg aria-hidden="true" width="105" height="78" class="absolute top-6 left-6 fill-slate-100">
+                    <path
+                        d="M25.086 77.292c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622C1.054 58.534 0 53.411 0 47.686c0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C28.325 3.917 33.599 1.507 39.324 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Zm54.24 0c-4.821 0-9.115-1.205-12.882-3.616-3.767-2.561-6.78-6.102-9.04-10.622-2.11-4.52-3.164-9.643-3.164-15.368 0-5.273.904-10.396 2.712-15.368 1.959-4.972 4.746-9.567 8.362-13.786a59.042 59.042 0 0 1 12.43-11.3C82.565 3.917 87.839 1.507 93.564 0l11.074 13.786c-6.479 2.561-11.677 5.951-15.594 10.17-3.767 4.219-5.65 7.835-5.65 10.848 0 1.356.377 2.863 1.13 4.52.904 1.507 2.637 3.089 5.198 4.746 3.767 2.41 6.328 4.972 7.684 7.684 1.507 2.561 2.26 5.5 2.26 8.814 0 5.123-1.959 9.19-5.876 12.204-3.767 3.013-8.588 4.52-14.464 4.52Z">
+                    </path>
+                </svg>
+                <blockquote class="relative">
+                    <p class="text-lg tracking-tight text-slate-700">
+                        Working with Thryve and Pranav was amazing and affordable. He was extremely helpful and receptive to feedback we provided. The website was extremely professional and up to a high standard with the little information we were able to provide him. I would definitely recommend.
+                    </p>
+                </blockquote>
+                <figcaption class="relative mt-6 flex items-center justify-between border-t border-slate-100 pt-6">
+                    <div>
+                        <div class="font-display text-base text-slate-900">Yaw B.</div>
+                        <div class="mt-1 text-sm text-slate-500">Founder @ InfoSavvy</div>
+                    </div>
+                    <div class="overflow-hidden rounded-full bg-slate-50">
+                        <img alt="" src="https://infosavvy.netlify.app/assets/infosavvylogo2-DPiCO72Y.png" class="h-14 w-14 object-cover" loading="lazy" width="56" height="56"/>
+                    </div>
+                </figcaption>
+            </figure>
+
+        </div>
 
         </div>
     </div>
